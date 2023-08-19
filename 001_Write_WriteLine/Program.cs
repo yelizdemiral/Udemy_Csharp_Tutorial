f4d1fe2171177443dcd27217956be4ebf378a533
yelizdemiral <yeliz8cak@gmail.com>
2023-08-19 15:09:06 +0300

    WrteLine ve Debugging derslerini işledim

---
+++ 001_Write_WriteLine/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba ");
+            Console.WriteLine("Sarman.");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am leaning Csharp");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 002_Debugging/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba");
+            Console.WriteLine("Bu projede debugging yani hata yakalama çalışması");
+            Console.WriteLine("yapacağız.");
+            
+        }
+    }
+}
