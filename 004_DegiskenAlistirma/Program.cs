449935d6e65486fb26b4bd75945e7e136e134175
yelizdemiral <yeliz8cak@gmail.com>
2023-08-27 22:05:47 +0300

    Değişken Alıştırmaları yaptım

---
+++ 004_DegiskenAlistirma/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DegiskenAlistirmalari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba");
+
+            Console.Write("İsminiz nedir ? ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Soyisminiz nedir ?");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("Yaşadığınız şehri giriniz");
+            string sehir = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("İsim : " + isim); // İsim Yeliz
+            Console.WriteLine("Soyisim : " +soyisim);
+            Console.WriteLine("Şehir : " + sehir);
+
+            Console.ReadLine(); 
+        }
+    }
+}
